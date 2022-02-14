656b6ab04bb4e42bf9fba411dda2feb07618f7aa
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-02-14 11:40:21 +0900

    firstcommit

---
+++ src/main/java/com/example/demo/JunitExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -0,0 +1,24 @@
+package junit.tutorial.ex01.e01;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest {
+
+	@Test
+	void test1() {
+		assertEquals("aaa",StringUtils.toSnakeCase("aaa"),"toSnakeCaseの引数がaaaのケース");
+	}
+	
+	@Test
+	void test2() {
+		assertEquals("hello_world",StringUtils.toSnakeCase("HelloWorld"),"toSnakeCaseの引数がHelloWorldのケース");
+	}
+	
+	@Test
+		void test3() {
+			assertEquals("practice_junit",StringUtils.toSnakeCase("practiceJunit"),"toSnakeCaseの引数がHelloWorldのケース");
+		}
+
+}
