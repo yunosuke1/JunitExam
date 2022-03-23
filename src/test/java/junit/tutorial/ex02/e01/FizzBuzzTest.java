2b6de7e8097caf5ad7aa5d045ad9c81f996ed384
yunosuke <yunosuke.ide@rakus-partners.co.jp>
2022-03-23 11:35:41 +0900

    update

--- src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
+++ src/test/java/junit/tutorial/ex02/e01/FizzBuzzTest.java
@@ -2,7 +2,7 @@ package junit.tutorial.ex02.e01;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -12,25 +12,11 @@ class FizzBuzzTest {
 	@Test
 	void test() {
 		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> list = new ArrayList<>();
-		list.add("1");
-		list.add("2");
-		list.add("Fizz");
-		list.add("4");
-		list.add("Buzz");
-		list.add("Fizz");
-		list.add("7");
-		list.add("8");
-		list.add("Fizz");
-		list.add("Buzz");
-		list.add("11");
-		list.add("Fizz");
-		list.add("13");
-		list.add("14");
-		list.add("FizzBuzz");
-		list.add("16");
+		List<String> expected = Arrays.asList("1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11",
+				"Fizz", "13", "14", "FizzBuzz", "16");
 		
-		assertIterableEquals(list,fizzBuzz.createFizzBuzzList(16),"createFizzBuzzメソッドの引数を16にして実行した場合");
+		
+		assertIterableEquals(expected,fizzBuzz.createFizzBuzzList(16),"createFizzBuzzメソッドの引数を16にして実行した場合");
 	}
 
 }
